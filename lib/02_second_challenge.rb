8ef8027e1a0fdde715360ff38e54f8d8331a6c15
arhamm ghazali <arhamm_ghazali93@hotmail.com>
2020-10-20 19:07:24 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,7 +1,7 @@
 def second_challenge
  epic_tradegy = {
     montague: {patriarch: {}, matriarch: {}, hero: {}, hero_friends: []},
-    capulet: {patriarch: {}, matriarch: {}, hero: {}, heroine_friends: []}
+    capulet: {patriarch: {}, matriarch: {}, heroine: {}, heroine_friends: []}
   }
 
 end
